c264d771894ffafbafbd9bc6c085307e52e8e831
jeeva parkkavan <jeeva.parkkavan@kalvium.community>
2023-02-22 11:41:33 +0530

    Jeeva made the changes

---
+++ pronote/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import  Editor from'./components/Editor'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Editor/>
+      </div>
+    );
+  }
+}
+
+export default App;
